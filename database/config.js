c3e454b0b255f1015d048986afc76742c0c5191b
Alex Nogueira <alex@devaction.com.br>
2020-09-02 06:05:54 -0300

    New project CodeTrap

---
+++ database/config.js
@@ -0,0 +1,9 @@
+var config = {
+    host: 'localhost', //O Host
+    user: 'alex', //Seu usuário aqui
+    password: 'Bruna@2012*', //Sua senha aqui
+    database: 'sheets', //O BD
+    multipleStatements: true
+};
+
+module.exports = config;
